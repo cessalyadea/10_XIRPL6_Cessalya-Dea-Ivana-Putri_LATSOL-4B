271c0136c5a0f0a4e2784ee5aae21a6b472825ce
LENOVO <LENOVO@LAPTOP-5DMSOMGE>
2021-08-02 18:08:12 +0700

    https://github.com/cessalyadea/10_XIRPL6_Cessalya-Dea-Ivana-Putri_LATSOL-4B.git

---
+++ src/belajar3b/Belajar3b.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package belajar3b;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Belajar3b {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+      
+        int sukupertama =5;
+        int selisih = 5;
+        int nilaiakhir = 75;
+        int updatenilai;
+        int totalnilai;
+      
+        
+        for (int a=0; a<=5; a++){
+            for (int i=a; i<5; i++){
+                
+                updatenilai=sukupertama;
+                sukupertama+=selisih;
+                
+            System.out.print(" "+updatenilai);   
+        }
+        System.out.println(" ");
+    }
+    
+}
+    }
+    
+
